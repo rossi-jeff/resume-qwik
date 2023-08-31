533fd4495d24a97ba5fe3d876bdacbdd16fccff7
Jeff Rossi <jrossi@intouchgps.com>
2023-08-31 12:12:43 -0400

    fetch references

--- src/routes/references/index.tsx
+++ src/routes/references/index.tsx
@@ -1,5 +1,18 @@
 import { component$ } from '@builder.io/qwik'
+import { routeLoader$ } from '@builder.io/qwik-city'
+import { GraphQlClient } from '~/lib/graph-ql-client'
+import { GET_REFERENCES_QUERY } from '~/graphql/queries/get-references'
+import { type Reference } from '~/types/reference.type'
+import { ReferenceList } from '~/components/reference-list/reference-list'
+
+export const useReferences = routeLoader$(async () => {
+	const client = new GraphQlClient()
+	const req = await client.query({ query: GET_REFERENCES_QUERY })
+	const res = await req.json()
+	return res.data.getReferences as Reference[]
+})
 
 export default component$(() => {
-	return <div>References works.</div>
+	const references = useReferences()
+	return <ReferenceList references={references.value} />
 })
