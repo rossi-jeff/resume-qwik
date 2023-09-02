e4f80fbac8ca2ba238efffe64a9f993c90d77ba3
Jeff Rossi <jrossi@intouchgps.com>
2023-09-02 10:36:12 -0400

    site layout

--- src/components/footer-bar/footer-bar.tsx
+++ src/components/footer-bar/footer-bar.tsx
@@ -1,62 +1,69 @@
-import { component$, $ } from "@builder.io/qwik";
-import { paletteNames } from "~/lib/palettes";
+import { component$, $ } from '@builder.io/qwik'
+import { paletteNames } from '~/lib/palettes'
 
 export interface FooterBarProps {
-  state: {
-    palette: string;
-    dark: boolean;
-  };
+	state: {
+		palette: string
+		dark: boolean
+	}
 }
 
 export const FooterBar = component$<FooterBarProps>((props) => {
-  const { palette, dark } = props.state;
+	const { palette, dark } = props.state
 
-  const paletteChanged = $((e: { target: { value: string } }) => {
-    const name = e.target.value;
-    props.state.palette = name;
-    document.body.setAttribute("data-theme", name);
-  });
+	const paletteChanged = $((e: { target: { value: string } }) => {
+		const name = e.target.value
+		props.state.palette = name
+		document.body.setAttribute('data-theme', name)
+	})
 
-  const darkChanged = $((e: { target: { checked: boolean } }) => {
-    const isDark = e.target.checked;
-    props.state.dark = isDark;
-    if (isDark) {
-      document.body.classList.add("dark");
-    } else {
-      document.body.classList.remove("dark");
-    }
-  });
-  return (
-    <div class="flex flex-wrap">
-      <div class="mr-2">
-        <label for="paletteName" class="mr-2">
-          Palette
-        </label>
-        <select
-          name="paletteName"
-          id="paletteName"
-          value={palette}
-          onChange$={paletteChanged}
-        >
-          {paletteNames.map((p, i) => (
-            <option key={i} value={p}>
-              {p}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div>
-        <input
-          type="checkbox"
-          name="darkCB"
-          checked={dark}
-          value={1}
-          onChange$={darkChanged}
-        />
-        <label for="darkCB" class="ml-1">
-          Dark
-        </label>
-      </div>
-    </div>
-  );
-});
+	const darkChanged = $((e: { target: { checked: boolean } }) => {
+		const isDark = e.target.checked
+		props.state.dark = isDark
+		if (isDark) {
+			document.body.classList.add('dark')
+		} else {
+			document.body.classList.remove('dark')
+		}
+	})
+	return (
+		<div class="footer-bar">
+			<div>
+				<h3>Jeff Rossi</h3>
+			</div>
+			<div>1506 Tuscaloosa Ave, Holly Hill Florida 32117</div>
+			<div>
+				<a href="mailto:inquiries@jeff-rossi.com">inquiries@jeff-rossi.com</a>
+			</div>
+			<div>
+				<label for="paletteName" class="mr-2">
+					Palette
+				</label>
+				<select
+					name="paletteName"
+					id="paletteName"
+					value={palette}
+					onChange$={paletteChanged}
+				>
+					{paletteNames.map((p, i) => (
+						<option key={i} value={p}>
+							{p}
+						</option>
+					))}
+				</select>
+			</div>
+			<div>
+				<input
+					type="checkbox"
+					name="darkCB"
+					checked={dark}
+					value={1}
+					onChange$={darkChanged}
+				/>
+				<label for="darkCB" class="ml-1">
+					Dark
+				</label>
+			</div>
+		</div>
+	)
+})
--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,29 +1,29 @@
-import { component$ } from "@builder.io/qwik";
-import { Link } from "@builder.io/qwik-city";
+import { component$ } from '@builder.io/qwik'
+import { Link } from '@builder.io/qwik-city'
 
 type LinkType = {
-  name: string;
-  path: string;
-};
+	name: string
+	path: string
+}
 
 export const Navigation = component$(() => {
-  const links: LinkType[] = [
-    { name: "Home", path: "/" },
-    { name: "Education", path: "/education" },
-    { name: "Employment", path: "/employment" },
-    { name: "Contact", path: "/contact" },
-    { name: "References", path: "/references" },
-    { name: "Testimonials", path: "/testimonials" },
-    { name: "Resume", path: "/download" },
-    { name: "Links", path: "/links" },
-  ];
-  return (
-    <div class="flex flex-wrap justify-between mx-2">
-      {links.map((link, idx) => (
-        <Link key={idx} href={link.path}>
-          {link.name}
-        </Link>
-      ))}
-    </div>
-  );
-});
+	const links: LinkType[] = [
+		{ name: 'Home', path: '/' },
+		{ name: 'Education', path: '/education' },
+		{ name: 'Employment', path: '/employment' },
+		{ name: 'Contact', path: '/contact' },
+		{ name: 'References', path: '/references' },
+		{ name: 'Testimonials', path: '/testimonials' },
+		{ name: 'Resume', path: '/download' },
+		{ name: 'Links', path: '/links' },
+	]
+	return (
+		<div class="flex flex-wrap justify-between">
+			{links.map((link, idx) => (
+				<Link key={idx} href={link.path}>
+					{link.name}
+				</Link>
+			))}
+		</div>
+	)
+})
